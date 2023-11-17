bcfe20d8e70314f445f8ebd55176c20b6aac264c
Tom McKenna <tom2mckenna@gmail.com>
2023-11-17 13:31:25 +0000

    Fix tests.

--- tests/test_gatherer.py
+++ tests/test_gatherer.py
@@ -1,6 +1,7 @@
-from proxybandit import Gatherer
+from proxybandit import Gatherer, ProxyList
 
 def test_gather():
     gatherer = Gatherer()
     proxy_list = gatherer.gather()
+    assert isinstance(proxy_list, ProxyList)
     assert len(proxy_list) > 0
--- tests/test_proxylist.py
+++ tests/test_proxylist.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 from proxybandit import Proxy, ProxyList
 
@@ -8,6 +9,16 @@ def test_to_csv():
         Proxy("1.1.1.1","2"),
         Proxy("1.1.1.1","3"),
     ]
+    expected_output = [
+        ["0","0","1.1.1.1","1","0","",""],
+        ["1","0","1.1.1.1","2","0","",""],
+        ["2","0","1.1.1.1","3","0","",""]
+    ]
     test_proxy_list = ProxyList(test_data)
-    test_proxy_list.to_csv('proxylist_test_to_csv.csv')
-    os.remove('proxylist_test_to_csv.csv')
+    test_proxy_list.to_csv('__test_to_csv.csv')
+    try:
+        with open('__test_to_csv.csv', 'r') as file:
+            csv_reader = csv.reader(file)
+            assert expected_output == list(csv_reader)
+    finally:
+        os.remove('__test_to_csv.csv')
